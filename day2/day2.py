7828e8dff72fb2e5ee20c01a530594536d81b9bf
david <david.t.fairbairn@gmail.com>
2021-12-25 23:00:12 -0500

    Day2pt2

--- day2/day2.py
+++ day2/day2.py
@@ -4,24 +4,25 @@ from dataclasses import dataclass
 
 @dataclass
 class BathState:
+    """ Bathymetric state for part 1 based on x/y/-z directions and commands in the X (forward) and Z (up/down) """
     x: int = 0
     y: int = 0
     depth: int = 0
 
     def _add(self, s):
+        """ Sum two bathstate vectors """
         self.x += s.x
         self.y += s.y
         self.depth += s.depth
 
     def move(self, movement):
-        """
-        Takes a bathymetric state and a movement operation string, returns new bathymetric state
-        """
+        """ Takes a bathymetric state and a movement operation string, returns new bathymetric state """
         move_state = BathState.parse_movement(movement)
         self._add(move_state)
 
     @staticmethod
     def parse_movement(movement_str: str):
+        """ Extract word and number from movement string, interpret as movement operation for a bathymetric state """
         try:
             dir, dist = movement_str.split(' ')
             dist = int(dist)
@@ -41,6 +42,46 @@ class BathState:
         return f"({self.x}, {self.y}, {self.depth})"
 
 
+@dataclass
+class BathStatePart2:
+    x: int = 0
+    aim: int = 0
+    depth: int = 0
+
+    def move(self, movement_str):
+        """
+        Takes a bathymetric state and a movement operation string, returns new bathymetric state
+        """
+        try:
+            dim, magnitude = movement_str.split(' ')
+            magnitude = int(magnitude)
+        except Exception as e:
+            print(f"Error: bad movement str")
+            raise Exception(e)
+        if dim == 'forward':
+            self.x += magnitude
+            self.depth += self.aim * magnitude
+        elif dim == 'up':
+            self.aim -= magnitude
+        elif dim == 'down':
+            self.aim += magnitude
+
+    def __repr__(self):
+        return f"({self.x}, {self.aim}, {self.depth})"
+
+
+def solve(movements, part="part2"):
+    if part == "part1":
+        s = BathState(0, 0, 0)
+    elif part == "part2":
+        s = BathStatePart2(0, 0, 0)
+    else:
+        print(f"Error: no part {part}")
+    for move in movements:
+        s.move(move)
+    return s.x * s.depth
+
+
 def main():
     if len(sys.argv) == 2:
         try:
@@ -50,12 +91,11 @@ def main():
             exit()
         # Parse movements
         movements = f.readlines()
-        s = BathState(0, 0, 0)
-        for move in movements:
-            # print(move)
-            s.move(move)
-        # print(s)
-        print(s.x*s.depth)
+        # If we're solving Part 1:
+        # solve(movements, part="part1")
+        # Otherwise, if solving Part 2:
+        sol = solve(movements, part="part2")
+        print(sol)
     else:
         print(f"Usage: \n{sys.argv[0]} <filename>")
         exit()
